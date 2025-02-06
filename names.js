5daf01985d053d66a1023f00062e86903573943b
Michael-Pellizzato <pellizzato.michael97@gmail.com>
2025-02-06 23:23:10 +0100

    add names

---
+++ names.js
@@ -0,0 +1,12 @@
+function nomeCognome(firstName, lastName){
+    return  {
+        nome : firstName,
+        cognome : lastName
+    }
+    
+}
+
+console.log(nomeCognome("a" , "b"))
+
+module.exports = nomeCognome;
+
